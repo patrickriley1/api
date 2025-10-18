b7e0519761422f843ea103820b96e5d97f647a60
Patrick Riley <righthandriley32@gmail.com>
2025-10-18 18:17:01 -0400

    initial commit

---
+++ api/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+import './App.css';
+import './Index.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main>
+      <div className="header">
+        <h1 className="title">API Project</h1>
+      </div>
+      <div className="content">
+        
+      </div>  
+    </main>
+  );
+}
+
+
+export default App;
